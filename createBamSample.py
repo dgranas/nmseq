eec50bfb6357343e040bfdb1e277790d9ab3bdef
dgranas <dgranas@ural.wustl.edu>
2018-06-20 12:27:51 -0500

    changed flag

--- createBamSample.py
+++ createBamSample.py
@@ -4,7 +4,7 @@
 Generates a bam_samples.txt file in current directory
 
 Usage:
-python CreateBamSample.py -d [bam_directory]
+python createBamSample.py -d [bam_directory]
 
 Optional arguments:
 -d [bam_directory]: directory containing bam files
@@ -25,44 +25,50 @@ Sample names can be given in 2 ways
 The csv file bam_samples.txt is created where each line has
 [bam filename], [sample name]
 
-This can then be used as input for CountEnds.py
+This can then be used as input for countEnds.py
 '''
 from builtins import input # to make py2 and py3 input compatible
 import os
 import argparse
 
-def find_bam_files(bam_dir = '.'):
+def find_bam_files(bam_dir):
     '''
-    Looks for bam files in a directory
-    Gets the sample names for each file
-    This is done either by parsing with a delimiter
+    Looks for bam files in bam_dir
+    Gets the sample names for each bam file
+    This is done either by breaking up the bam filename with a delimiter
     Or by having the user enter the name for each file
     Returns list of tuples [(bamfile, sample)]
     '''
     # make sure there isn't already a bam_sample file in current directory
     if os.path.exists('bam_sample.txt'):
         raise SystemExit('There is already a bam_sample.txt file in the directory')
+
     bam_samples = []
-    parse_asked = False # once we ask about parsing, set to True so don't ask again
-    parse_flag = False # set to True if we are parsing all the files
+    parse_flag = 0 # set to 1 if we are using a delimiter, otherwise gets set to 2
 
     for filename in os.listdir(bam_dir):
         if filename.endswith('.bam'):
-            print(filename)
-            if not parse_asked:
-                parse_asked = True
+            print('bam file: {}'.format(filename))
+            if not parse_flag:
+                parse_flag = 1
 
-                delim = input('If you want to name the sample by breaking up the filename using a delimiter,\nenter the delimiter to use (or press Enter to skip this): ')
+                print('If you want to name the sample by breaking up the filename ' 
+                      'using a delimiter,\nenter the delimiter to use (or press '
+                      'Enter to skip this)')
+
+                delim = input('Delimiter: ')
                 if delim:
-                    print('Enter the fields you want to include as comma-separated numbers, starting with 1')
-                    print('For example, enter 2,3,4 to get WT_BSA_input from NmSeq_WT_BSA_input_filtered.bam')
+                    parse_flag = 2
+                    print('Enter the fields you want to include as comma-separated '
+                          'numbers, starting with 1')
+                    print('For example, enter 2,3,4 to get WT_BSA_input from '
+                          'NmSeq_WT_BSA_input_filtered.bam')
                     
                     fields = []
                     while not fields:
                         fields = [int(i) for i in input('Enter fields to use: ').split(',')]
-                    parse_flag = True
 
-            if parse_flag:
+            if parse_flag == 2:
                 sample = '_'.join([filename.split(delim)[i-1] for i in fields])
             else:
                 sample = input('Enter sample name (or press Enter to exclude): ')
@@ -78,9 +84,9 @@ def write_bam_samples(bam_samples):
     '''
     bam_samples = sorted(bam_samples) # sort by filename
 
-    print('\nEnter the desired order for the samples using comma-separated positions')
-    print('For example entering 3,4,1,2 for samples A,B,C,D would get the order C,D,A,B')
-    print('Samples: ')
+    print('\nEnter the desired order for the samples using comma-separated positions\n'
+          'For example entering 3,4,1,2 for samples A,B,C,D would get the order C,D,A,B\n'
+          'Samples: ')
     for i, bam_sample in enumerate(bam_samples):
         print('{}\t{}'.format(i+1, bam_sample[1]))
     order = input('Desired order (or press Enter to keep this order): ')
@@ -104,12 +110,11 @@ def main():
         help='directory containing bam files')
     args = parser.parse_args()
 
-    if not args.directory:
+    if not args.directory: # default is to use current directory
         args.directory = '.'
 
     bam_samples = find_bam_files(args.directory)
 
-    # case were no bam files were found
     if not bam_samples:
         raise SystemExit('No bam files found in directory {}'.format(args.directory))
 
